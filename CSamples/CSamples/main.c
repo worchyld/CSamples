cf98b28efa4eab7826543adb53158c1d03e1332a
AD <silentflute.ad@gmail.com>
2024-04-29 20:18:30 +0100

    Adding some more example code

--- CSamples/CSamples/main.c
+++ CSamples/CSamples/main.c
@@ -15,5 +15,23 @@ int main(int argc, const char * argv[]) {
     
     // multi-line
     printf("Hello World!\nI am learning C.\nAnd it is awesome!\n");
+    printf("first\tsecond\\backslash\n");
+    
+    // variables
+    int myNum = 15;             // Integer
+    float myFloatNum = 5.99;   // Floating point number
+    char myLetter = 'D';       // Character
+    
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    
+    int myOtherNum = 23;
+
+    // Assign myOtherNum (23) to myNum
+    myNum = myOtherNum;
+    
+    printf("%d\n", myNum);
+    
     return 0;
 }
