5cc1700e96bab868750c22ba825d73e1a899a57d
AD <silentflute.ad@gmail.com>
2024-04-29 20:08:02 +0100

    First initial C sample

---
+++ CSamples/CSamples/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CSamples
+//
+//  Created by Amarjit on 29/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
